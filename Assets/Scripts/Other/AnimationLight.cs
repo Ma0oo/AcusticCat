d2920155cec2f4542b04c0e6849d5339ccab3e1b
Ma0oo <belkapbigiy@gmail.com>
2020-12-30 17:08:05 +0300

    1) Анимация  света. 2) Добавлена пыль в комнатах. 3) Добавлена стрелка над котэ 4) Добавлены скрипты для GUI главного меню, но без прекрас.

---
+++ Assets/Scripts/Other/AnimationLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimationLight : MonoBehaviour
+{
+    private Light _light;
+    [SerializeField] private float _minusIntensive;
+    [SerializeField] private float _plusIntensive;
+    [SerializeField] private float _minDuration;
+    [SerializeField] private float _maxDuration;
+
+    private float _duration;
+    private float _maxIntensive;
+    private float _minIntensive;
+
+    private void Awake()
+    {
+        _light = GetComponentInChildren<Light>();
+        _maxIntensive = Random.Range(_light.intensity, _light.intensity + _plusIntensive);
+        _minIntensive = Random.Range(_light.intensity, _light.intensity - _minusIntensive);
+        _light.intensity = _minIntensive;
+        _duration = Random.Range(_minIntensive, _maxIntensive);
+        _light.DOIntensity(_maxIntensive, _duration).SetLoops(-1, LoopType.Yoyo);
+    }
+
+}
---
+++ Assets/Scripts/Other/Arrow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private float _diferentScale;
+    [SerializeField] private float _durrationOneLoop;
+    [SerializeField] private float _distaceMin;
+    [SerializeField] private float _distaceMax;
+
+    private Camera _camera;
+    private SpriteRenderer _sprite;
+
+    void Start()
+    {
+        transform.DOScale(transform.localScale.x + _diferentScale, _durrationOneLoop).SetLoops(-1, LoopType.Yoyo);
+        _camera = Camera.main;
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Color color = _sprite.color;
+        float distaceToCamera = Vector3.Distance(transform.position, _camera.transform.position);
+        if (distaceToCamera < _distaceMin)
+            color.a = 0;
+        else if (distaceToCamera > _distaceMax)
+            color.a = 1;
+        else
+            color.a = distaceToCamera / _distaceMax;
+
+
+        _sprite.color = color;
+    }
+
+}
